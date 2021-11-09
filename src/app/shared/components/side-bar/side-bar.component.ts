3611003c860253446dce5961bb3be5956e2f631d
“ArenasAgustin” <arenasagustin7@gmail.com>
2021-11-09 00:13:55 -0300

    mas modules

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface defaultOptions {
+  icon: string;
+  name: string;
+  router: string[];
+  query?: {
+    hola: string;
+  }
+}
+
+interface accessLink {
+  name: string;
+  icon: string;
+}
+
+interface mainMenuInterface {
+  defaultOptions?: defaultOptions[],
+  accessLink?: accessLink[],
+}
+
+interface customOptionsInterface {
+  name: string;
+  router: string[];
+}
 
 @Component({
   selector: 'app-side-bar',
@@ -7,9 +32,72 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
-  constructor() { }
+  mainMenu: mainMenuInterface = {};
+  customOptions: customOptionsInterface[] | [] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.mainMenu = {
+      defaultOptions: [
+        {
+          name: 'Home',
+          icon: 'uil uil-estate',
+          router: ['/', 'auth']
+        },
+        {
+          name: 'Buscar',
+          icon: 'uil uil-search',
+          router: ['/', 'history']
+        },
+        {
+          name: 'Tu biblioteca',
+          icon: 'uil uil-chart',
+          router: ['/', 'favorites'],
+          query: { hola: 'mundo' }
+        }
+      ],
+
+      accessLink: [
+        {
+          name: 'Crear lista',
+          icon: 'uil-plus-square'
+        },
+        {
+          name: 'Canciones que te gustan',
+          icon: 'uil-heart-medical'
+        }
+      ]
+    }
+
+    this.customOptions = [
+      {
+        name: 'Mi lista º1',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º2',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º3',
+        router: ['/']
+      },
+      {
+        name: 'Mi lista º4',
+        router: ['/']
+      }
+    ]
   }
 
+  goTo($event: any): void {
+    this.router.navigate(['/', 'favorites'], {
+      queryParams: {
+        key1: 'value1',
+        key2: 'value2',
+        key3: 'value3'
+      }
+    })
+    console.log($event)
+  }
 }
